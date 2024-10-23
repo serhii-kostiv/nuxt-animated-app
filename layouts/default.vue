59002015cb7c29d1dc62684fc553c0b8dd646512
kraiviks <kraiviks@gmail.com>
2024-10-23 14:03:06 +0300

    feat: added page-blur, when page loading

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
-	<div class="w-screen h-screen overflow-hidden">
+	<div
+		class="w-screen h-screen overflow-hidden"
+		:class="{ 'page-blur': !isLoading, 'page-clear': isLoading }"
+	>
 		<Header />
 		<main class="w-full h-full">
 			<slot />
 		</main>
 	</div>
 </template>
+
+<script lang="ts" setup>
+const isLoading = ref<boolean>(false);
+
+onMounted(() => {
+	isLoading.value = true;
+});
+</script>
